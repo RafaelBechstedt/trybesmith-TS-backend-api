839e7b448062128aa24975796806c2c5cbfcefd7
Rafael <bechstedt@hotmail.com>
2023-05-10 03:11:31 -0300

    Adiciona returns que tinha esquecido nas validacoes

--- src/middlewares/validateProduct.ts
+++ src/middlewares/validateProduct.ts
@@ -4,13 +4,13 @@ const validateName = async (req: Request, res: Response, next: NextFunction) => 
   const product = req.body;
   const { name } = product;
   if (!name) {
-    res.status(400).json({ message: '"name" is required' });
+    return res.status(400).json({ message: '"name" is required' });
   }
   if (typeof name !== 'string') {
-    res.status(422).json({ message: '"name" must be a string' });
+    return res.status(422).json({ message: '"name" must be a string' });
   }
   if (name.length < 3) {
-    res.status(422).json({ message: '"name" length must be at least 3 characters long' });
+    return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
   }
   next();
 };
@@ -19,13 +19,13 @@ const validateAmount = async (req: Request, res: Response, next: NextFunction) =
   const product = req.body;
   const { amount } = product;
   if (!amount) {
-    res.status(400).json({ message: '"amount" is required' });
+    return res.status(400).json({ message: '"amount" is required' });
   }
   if (typeof amount !== 'string') {
-    res.status(422).json({ message: '"amount" must be a string' });
+    return res.status(422).json({ message: '"amount" must be a string' });
   }
   if (amount.length < 3) {
-    res.status(422).json({ message: '"amount" length must be at least 3 characters long' });
+    return res.status(422).json({ message: '"amount" length must be at least 3 characters long' });
   }
   next();
 };
